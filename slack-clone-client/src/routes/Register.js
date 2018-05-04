d2ef6989a928cd42acb85db2afd9c1b5de6e3434
Benjamin Huang <bhuang@rms.com>
2018-05-04 15:59:03 -0700

    register

--- slack-clone-client/src/routes/Register.js
+++ slack-clone-client/src/routes/Register.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Input, Container, Header } from 'semantic-ui-react';
+import { Button, Input, Container, Header } from 'semantic-ui-react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
 class Register extends React.Component {
     state = {
         username: '',
         email: '',
         password: ''
-    }
+    };
+
     onChange = (e) => {
-        const {name, value} = e.target;
+        const { name, value } = e.target;
         //[name] is the value of name
         // name = "email"
-        this.setState({[name]:value})
-    }
+        this.setState({ [name]: value })
+    };
+
+    onSubmit = async (e) => {
+        console.log(this.state);
+        const response = await this.props.mutate({
+            variables: this.state,
+        });
+        console.log(response);
+    };
+
     render() {
         const { username, email, password } = this.state;
         return (
@@ -20,10 +32,17 @@ class Register extends React.Component {
                 <Header as="h2">Register</Header>
                 <Input name="username" onChange={this.onChange} value={username} placeholder="Username" fluid />
                 <Input name="email" onChange={this.onChange} value={email} placeholder="Email" fluid />
-                <Input name="password"  onChange={this.onChange} value={password} type="password" placeholder="Password" fluid />
+                <Input name="password" onChange={this.onChange} value={password} type="password" placeholder="Password" fluid />
+                <Button onClick={this.onSubmit}>Submit</Button>
             </Container>
         );
     }
 }
 
-export default Register;
+const registerMutation = gql`
+    mutation($username: String!, $email: String!, $password: String!){
+        register(username: $username, email: $email, password: $password)
+    }
+`;
+
+export default graphql(registerMutation)(Register);
