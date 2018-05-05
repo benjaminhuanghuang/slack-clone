bf0e51003c3077da95cb29100fbd44f8009c35f0
Benjamin Huang <bhuang@rms.com>
2018-05-05 16:02:34 -0700

    creat team

--- slack-clone-server/resolvers/team.js
+++ slack-clone-server/resolvers/team.js
@@ -1,12 +1,20 @@
+import formatErrors from '../formatErrors';
+
 export default {
-    Mutation: {
-        createTeam: async (parent, args, { models }) => {
-            try {
-                await models.Team.create(args);
-                return true;
-            } catch (err) {
-                return false;
-            }
-        }
-    }
+  Mutation: {
+    createTeam: async (parent, args, { models, user }) => {
+      try {
+        await models.Team.create({ ...args, owner: user.id });
+        return {
+          ok: true,
+        };
+      } catch (err) {
+        console.log(err);
+        return {
+          ok: false,
+          errors: formatErrors(err),
+        };
+      }
+    },
+  },
 };
--- slack-clone-server/schemas/team.js
+++ slack-clone-server/schemas/team.js
@@ -1,11 +1,16 @@
 export default `
-    type Team {
-        owner: User!
-        member: [User!]!
-        channels: [Channel!]!
-    }
+  type Team {
+    owner: User!
+    members: [User!]!
+    channels: [Channel!]!
+  }
+  
+  type CreateTeamResponse {
+    ok: Boolean!
+    errors: [Error!]
+  }
 
-    type Mutation {
-        createTeam(name: String!): Boolean!
-    }
-`
+  type Mutation {
+    createTeam(name: String!): CreateTeamResponse!
+  }
+`;
